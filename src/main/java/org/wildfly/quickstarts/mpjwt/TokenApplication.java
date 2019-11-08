9ad41c07a8c0c20c92b6a8b1cf440acd6c7fa26b
Darran Lofthouse <darran.lofthouse@jboss.com>
2019-11-08 20:38:26 +0000

    Activate MicroProfile JWT for the endpoint.

--- src/main/java/org/wildfly/quickstarts/mpjwt/TokenApplication.java
+++ src/main/java/org/wildfly/quickstarts/mpjwt/TokenApplication.java
@@ -3,5 +3,8 @@ package org.wildfly.quickstarts.mpjwt;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import org.eclipse.microprofile.auth.LoginConfig;
+
 @ApplicationPath("/rest")
+@LoginConfig(authMethod="MP-JWT", realmName="MP JWT Realm")
 public class TokenApplication extends Application {}
