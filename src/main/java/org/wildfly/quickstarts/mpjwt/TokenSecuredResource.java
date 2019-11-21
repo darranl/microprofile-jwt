8cb2f516e972d3161f97cab99ed41e32a98a8308
Darran Lofthouse <darran.lofthouse@jboss.com>
2019-11-21 17:08:18 +0000

    Add a new endpoint that requires the 'Subscriber' role and activate role based access control.

--- src/main/java/org/wildfly/quickstarts/mpjwt/TokenSecuredResource.java
+++ src/main/java/org/wildfly/quickstarts/mpjwt/TokenSecuredResource.java
@@ -2,10 +2,14 @@ package org.wildfly.quickstarts.mpjwt;
 
 import java.security.Principal;
 
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.SecurityContext;
+
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
 @Path("/secured")
 public class TokenSecuredResource {
@@ -17,6 +21,20 @@ public class TokenSecuredResource {
         String caller = principal == null ? "anonymous" : principal.getName();
 
         return "Hello " + caller;
+    }
+
+    @Inject
+    JsonWebToken jwt;
+
+    @GET()
+    @Path("/subscription")
+    @RolesAllowed({"Subscriber"})
+    public String helloRolesAllowed(@Context SecurityContext ctx) {
+        Principal caller =  ctx.getUserPrincipal();
+        String name = caller == null ? "anonymous" : caller.getName();
+        boolean hasJWT = jwt.getClaimNames() != null;
+        String helloReply = String.format("hello + %s, isSecure: %s, authScheme: %s, hasJWT: %s", name, ctx.isSecure(), ctx.getAuthenticationScheme(), hasJWT);
+        return helloReply;
     }
 
 }
